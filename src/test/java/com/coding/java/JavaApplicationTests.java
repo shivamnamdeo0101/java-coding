2e88a5fc4a22f9b9fe863623ee2ee78236a6603e
shinamde <shivam.namdeo@publicissapient.com>
2025-10-17 19:08:12 +0530

    first commit

---
+++ src/test/java/com/coding/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
