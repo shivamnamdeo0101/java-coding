306e775ab4a5cf04165785af4f3c6c09e141bd1a
shinamde <shivam.namdeo@publicissapient.com>
2025-10-19 19:09:37 +0530

    LargestNum

---
+++ src/main/java/com/coding/java/Java_4_LargestNum.java
@@ -0,0 +1,45 @@
+package com.coding.java;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+interface LargestNum{
+    void printLargestNum(int[] arr);
+}
+
+class LargestNumStreamCollection implements LargestNum{
+
+    @Override
+    public void printLargestNum(int[] arr) {
+        int max = Arrays.stream(arr).max().getAsInt();
+        System.out.println(max);
+        List<Integer> li = Arrays.stream(arr).boxed().toList();
+        int maxby = Collections.max(li);
+        System.out.println(maxby);
+    }
+}
+
+class LargestNumArrayLoop implements LargestNum{
+
+    @Override
+    public void printLargestNum(int[] arr) {
+        int max = arr[0];
+
+        for(int num:arr){
+            if(num > max){
+                max = num;
+            }
+        }
+        System.out.println(max);
+    }
+}
+
+public class Java_4_LargestNum {
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,632,21,213,4,55,5,6,6};
+        new LargestNumArrayLoop().printLargestNum(arr);
+        new LargestNumStreamCollection().printLargestNum(arr);
+    }
+}
